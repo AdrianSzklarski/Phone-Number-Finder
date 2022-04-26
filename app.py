c7d6c94279bf03f0e4eab6ddafe8d1b5675d3a11
AdrianSzklarski <szklarski.adrian@gmail.com>
2022-04-26 08:44:59 +0200

    Added form without functional send, yet

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import random
+# flask run --host=0.0.0.0 --port=80
 
 app = Flask(__name__)
 
 counter = 0
 array = []
-index = random.randint(1, 2000)
+index = random.randint(1, 10)
 while counter < index:
     array.append(random.randint(111111111, 999999999))
     counter += 1
 
 
+
 @app.route('/')
 def homepage():
+    return render_template("index.html", len=counter, array=array, index=index)
 
-        return render_template("index.html", len=counter, array=array, index=index-1)
+
+@app.route('/', methods=('GET', 'POST'))
+def click():
+    name = request.form['name']
+    return render_template('/', name=name)
+
+
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, debug=True)
+    app.run(host='0.0.0.0', port=5001)
+
+
+
 
 
